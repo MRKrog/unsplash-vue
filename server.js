1ef4ec4de1f245baa521d79b7f640cca82ccaa18
Michael Krog <michaelryankrog@gmail.com>
2019-06-03 18:06:41 -0600

    udpates

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
-const express = require('express')
-const serveStatic = require('serve-static')
-const path = require('path')
-// create the express app
-const app = express()
-// create middleware to handle the serving the app
-app.use("/", serveStatic ( path.join (__dirname, '/dist') ) )
-// Create default port to serve the app on
-const port = process.env.PORT || 5000
-app.listen(port)
-// Log a feedback that this is actually running
-console.log('Server started on port ' + port)
+var express = require('express');
+const path = require('path');
+var serveStatic = require('serve-static');
+app = express();
+app.use(serveStatic(__dirname + "/dist"));
+var port = process.env.PORT || 5000;
+app.listen(port);
+console.log('server started '+ port);
